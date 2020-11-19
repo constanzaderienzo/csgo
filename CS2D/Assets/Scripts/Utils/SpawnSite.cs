5034325cda7ef8cce767ec59de6ee0ed9cac47a8
Constanza De Rienzo <cderienzo@itba.edu.ar>
2020-11-19 13:48:49 -0300

    Spawn sites unv files

---
+++ CS2D/Assets/Scripts/Utils/SpawnSite.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSite
+{
+
+    public float minX, maxX, minZ, maxZ;
+
+    public SpawnSite(float minX, float maxX, float minZ, float maxZ)
+    {
+        this.minX = minX;
+        this.maxX = maxX;
+        this.minZ = minZ;
+        this.maxZ = maxZ;
+    }
+}
